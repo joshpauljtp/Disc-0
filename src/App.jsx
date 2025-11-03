8d45e6d13dec8e7e331e4f762ebe727b7e229281
Joshua Paul <joshpauljtp@gmail.com>
2025-11-03 21:59:21 +0100

    React router support

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,12 @@
-import { useEffect, useRef, useState } from "react";
-import AlbumList from "./AlbumList";
+import { BrowserRouter } from "react-router";
 import "./App.css";
-import disc0Logo from "./assets/disc-0.svg";
-import HiddenYoutubePlayer from "./HiddenYoutubePlayer";
-import Player from "./Player";
-import { useRouter } from "./useRouter";
+import MainLogic from "./MainLogic";
 
 function App() {
-  const playerRef = useRef(null);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  const [selectedTrack, setSelectedTrack] = useState("");
-
-  const [progress, setProgress] = useState(0);
-  const [duration, setDuration] = useState(0);
-
-  const { navigate, path } = useRouter();
-
-  const onReadyCallback = (event) => {
-    playerRef.current = event.target;
-  };
-
-  const togglePlay = () => {
-    const player = playerRef.current;
-    if (!player) return;
-    isPlaying ? player.pauseVideo() : player.playVideo();
-    setIsPlaying(!isPlaying);
-  };
-
-  // update progress every 500ms while playing
-  useEffect(() => {
-    let interval;
-    if (isPlaying) {
-      interval = setInterval(() => {
-        const player = playerRef.current;
-        if (player) {
-          const current = player.getCurrentTime();
-          const total = player.getDuration();
-          setProgress(current);
-          setDuration(total);
-        }
-      }, 500);
-    } else {
-      clearInterval(interval);
-    }
-    return () => clearInterval(interval);
-  }, [isPlaying]);
-
-  const handleSeek = (e) => {
-    const player = playerRef.current;
-    if (!player) return;
-    const value = parseFloat(e.target.value);
-    player.seekTo(value, true);
-
-    setProgress(value);
-  };
-
-  const onSelectTrack = (s) => {
-    setSelectedTrack(s);
-    navigate(`/track/${s}`);
-  };
-
-  const showPlayer = path.startsWith("/track/");
-
   return (
-    <>
-      <header onClick={() => navigate("/")}>
-        <img src={disc0Logo} alt="Disc-0" />
-      </header>
-      <main>
-        {showPlayer ? (
-          <Player
-            togglePlay={togglePlay}
-            isPlaying={isPlaying}
-            handleSeek={handleSeek}
-            duration={duration}
-            progress={progress}
-          />
-        ) : (
-          <AlbumList setSelectedTrack={onSelectTrack} />
-        )}
-        <HiddenYoutubePlayer
-          selectedTrack={selectedTrack}
-          setIsPlaying={setIsPlaying}
-          onReadyCallback={onReadyCallback}
-        />
-      </main>
-    </>
+    <BrowserRouter>
+      <MainLogic />
+    </BrowserRouter>
   );
 }
 
---
+++ src/MainLogic.jsx
@@ -0,0 +1,106 @@
+import { useEffect, useMemo, useRef, useState } from "react";
+import { useLocation, useNavigate } from "react-router";
+import AlbumList from "./AlbumList";
+import "./App.css";
+import disc0Logo from "./assets/disc-0.svg";
+import HiddenYoutubePlayer from "./HiddenYoutubePlayer";
+import Player from "./Player";
+
+function MainLogic() {
+  const location = useLocation();
+  const playerRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const [selected, setSelected] = useState({
+    album: "",
+    track: "",
+  });
+
+  const [progress, setProgress] = useState(0);
+  const [duration, setDuration] = useState(0);
+
+  const navigate = useNavigate();
+
+  const onReadyCallback = (event) => {
+    playerRef.current = event.target;
+  };
+
+  const togglePlay = () => {
+    const player = playerRef.current;
+    if (!player) return;
+    isPlaying ? player.pauseVideo() : player.playVideo();
+    setIsPlaying(!isPlaying);
+  };
+
+  // update progress every 500ms while playing
+  useEffect(() => {
+    let interval;
+    if (isPlaying) {
+      interval = setInterval(() => {
+        const player = playerRef.current;
+        if (player) {
+          const current = player.getCurrentTime();
+          const total = player.getDuration();
+          setProgress(current);
+          setDuration(total);
+        }
+      }, 500);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isPlaying]);
+
+  const handleSeek = (v) => {
+    const player = playerRef.current;
+    if (!player) return;
+    const value = parseFloat(v);
+    player.seekTo(value, true);
+
+    setProgress(value);
+  };
+
+  const onSelectAlbum = (s) => {
+    setSelected((p) => ({ ...p, album: s }));
+    let path = `/album/${s}`;
+    if (selected.track) path += `/track/${selected.track}`;
+    navigate(path);
+  };
+
+  const showPlayer = useMemo(
+    () => location.pathname.startsWith("/album/"),
+    [location]
+  );
+
+  return (
+    <>
+      <header onClick={() => navigate("/")}>
+        <img src={disc0Logo} alt="Disc-0" />
+      </header>
+      <main>
+        {showPlayer ? (
+          <Player
+            albumIndex={selected.album}
+            togglePlay={togglePlay}
+            isPlaying={isPlaying}
+            handleSeek={handleSeek}
+            duration={duration}
+            progress={progress}
+            setSelected={setSelected}
+          />
+        ) : (
+          <AlbumList onSelectAlbum={onSelectAlbum} />
+        )}
+        {selected.track && (
+          <HiddenYoutubePlayer
+            selectedTrack={selected.track}
+            setIsPlaying={setIsPlaying}
+            onReadyCallback={onReadyCallback}
+          />
+        )}
+      </main>
+    </>
+  );
+}
+
+export default MainLogic;

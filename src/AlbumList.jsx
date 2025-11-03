26d44b6ed9282a4f78d44b8b082110b65beccd9f
Joshua Paul <joshpauljtp@gmail.com>
2025-11-03 21:55:30 +0100

    GSAP scrolltrigger

--- src/AlbumList.jsx
+++ src/AlbumList.jsx
@@ -1,20 +1,45 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useRef } from "react";
 import { ALBUM_LIST } from "./constants";
 import Disc from "./Disc";
 
-export default function AlbumList({ setSelectedTrack }) {
-  const onClick = (id) => {
-    setSelectedTrack(id);
-  };
+export default function AlbumList({ onSelectAlbum }) {
+  gsap.registerPlugin(useGSAP, ScrollTrigger);
+
+  const container = useRef();
+
+  useGSAP(
+    () => {
+      const boxes = gsap.utils.toArray(".card");
+      boxes.forEach((box) => {
+        gsap.to(box, {
+          scale: 0.5,
+          opacity: 0.5,
+          transformOrigin: "bottom center",
+          scrollTrigger: {
+            trigger: box,
+            start: "top top",
+            end: "bottom top",
+            scrub: true,
+            markers: true,
+          },
+        });
+      });
+    },
+    { scope: container }
+  );
 
   return (
-    <>
+    <div ref={container}>
       {ALBUM_LIST.map((album, index) => (
         <Disc
           albumArt={album.img}
           key={index}
-          onClick={() => onClick(album.tracks[0].youtubeId)}
+          onClick={() => onSelectAlbum(index)}
         />
       ))}
-    </>
+    </div>
   );
 }

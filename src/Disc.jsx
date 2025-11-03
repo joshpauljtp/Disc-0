f3178d93df0da7385184d181c03d7806bfbe1685
Joshua Paul <joshpauljtp@gmail.com>
2025-11-03 11:58:56 +0100

    Back button

--- src/Disc.jsx
+++ src/Disc.jsx
@@ -1,8 +1,11 @@
 import DiscImage from "./assets/disc.svg";
+import { useRouter } from "./useRouter";
 export default function Disc(props) {
+  const { goBack } = useRouter();
   return (
     <div className="card" onClick={props.onClick}>
       <img src={DiscImage} alt="Disc-0" className="disc" />
+      {props.showBackButton && <span onClick={goBack}>🡐</span>}
       <img src={props.albumArt} alt="" className="albumArt" />
     </div>
   );
